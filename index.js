9c7fcfb5bfe959903bfa6c1e8ea057237b4e7493
John McLear <john@mclear.co.uk>
2013-01-21 17:18:12 +0000

    yay

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var eejs = require('ep_etherpad-lite/node/eejs');
+console.log("WWUUU");
 
+/*
 exports.eejsBlock_timesliderEditbarRight = function (hook_name, args, cb) { 
-  args.content = eejs.require("ep_timesliderdiff/templates/timesliderDiff.ejs") + args.content;
+  // args.content = eejs.require("ep_themes/templates/themes.ejs") + args.content;
   return cb();
 }
 
 exports.eejsBlock_timesliderTop = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_timesliderdiff/templates/timesliderDiffForm.ejs");
+  // args.content = args.content + eejs.require("ep_themes/templates/themesForm.ejs");
   return cb();
 }
 
 exports.eejsBlock_timesliderStyles = function (hook_name, args, cb) {
-  args.content = args.content + "<link rel='stylesheet' href='/static/plugins/ep_timesliderdiff/static/css/diffview.css' type='text/css'>";
+  args.content = args.content + "<link rel='stylesheet' href='/static/plugins/ep_themes/static/css/diffview.css' type='text/css'>";
   return cb();
 }
+*/
 
 exports.eejsBlock_timesliderBody = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/difflib.js");
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/diffview.js");
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/timesliderDiff.js");
+  args.content = args.content + "<script>"+eejs.require("ep_themes/static/js/themes.js") + "</script>";
   return cb();
 }
 
+exports.eejsBlock_body = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_themes/templates/scripts.ejs");
+  return cb();
+}
+
+exports.postAceInit = function(one, two){
+  return cb();
+}
