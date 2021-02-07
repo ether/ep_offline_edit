4afb8924e6cf724202e5aa7739dc3e47bb6e5826
John McLear <john@mclear.co.uk>
2021-02-07 15:12:43 +0000

    Still not working but some more ideas here

--- static/js/serviceWorker.js
+++ static/js/serviceWorker.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const version = 'v1::';
+const version = '9::';
 
 self.addEventListener('install', (event) => {
   console.log('WORKER: install event in progress.');
   event.waitUntil(
       caches.open(`${version}fundamentals`)
-          .then((cache) => cache.addAll(['/']))
-          .then(() => {
-            console.log('WORKER: install completed');
-          })
+          .then(
+              (cache) => cache.addAll(['/'])
+          )
+          .then(
+              () => {
+                console.log('WORKER: install completed');
+              }
+          )
   );
 });
 
@@ -24,9 +28,36 @@ self.addEventListener('fetch', (event) => {
       caches
           .match(event.request)
           .then((cached) => {
-            const unableToResolve = () => {
-              console.log('WORKER: fetch request failed in both cache and network.');
-              return new Response('<h1>Service Unavailable</h1>', {
+            const unableToResolve = async () => {
+              console.log('WORKER FML: fetch request failed in both cache and network.');
+              const localforageUrl =
+              '../static/plugins/ep_offline_edit/static/js/lib/localForage.js';
+              /*
+              if (event.request.url.indexOf('/p/') !== -1) {
+
+                // globals localforage
+                const url = new URL(event.request.url);
+                const padId = url.pathname.split('/')[url.pathname.split.length];
+                console.log(padId, event.request);
+                // load localstorage
+                const script = document.createElement('script');
+                script.src = localforageUrl;
+                document.head.appendChild(script);
+                setTimeout(async () => {
+                  const padHTML = await localforage.get(padId);
+                  console.log(padHTML);
+                }, 1000);
+              }
+              */
+              console.log('new response...');
+              return new Response('<h1>Etherpad Unavailable</h1>' +
+                `<script type="text/javascript" src="${localforageUrl}"></script>` +
+                `<script type="text/javascript">
+                localforage.getItem('ERgOOQZETqB8fkI-qwui', function(err, value){
+                  console.log(value);
+                });
+                </script>
+              `, {
                 status: 503,
                 statusText: 'Service Unavailable',
                 headers: new Headers({
@@ -55,6 +86,25 @@ self.addEventListener('fetch', (event) => {
             console.log(
                 'WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
             return cached || networked;
+          })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('WORKER: activate event in progress.');
+
+  event.waitUntil(
+      caches.keys()
+          .then((keys) => Promise.all(
+              keys
+                  .filter((key) => !key.startsWith(version)
+                  )
+                  .map((key) => caches.delete(key)
+                  )
+          )
+          )
+          .then(() => {
+            console.log('WORKER: activate completed.');
           })
   );
 });
